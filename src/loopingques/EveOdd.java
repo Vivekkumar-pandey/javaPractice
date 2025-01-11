0fd0f0db9192149d35c5a93fcac2f72ec91deae3
Vivek Kumar <vpandey442003@gmail.com>
2025-01-12 01:46:44 +0530

    even odd program solve

---
+++ src/loopingques/EveOdd.java
@@ -0,0 +1,12 @@
+package loopingques;
+
+public class EveOdd {
+    public static void main(String[] args) {
+        int num=31;
+        if(num%2==0){
+            System.out.println("it is the even number");
+        }else{
+            System.out.println("it is odd number");
+        }
+    }
+}
